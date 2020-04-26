dc1588108cace2a9110bb8a5e81573105e2c65ba
Ivan <ivanzhao8@gmail.com>
2020-04-26 00:15:24 -0700

    Reorganized file structure, attempted to run all files through main

---
+++ code/main.py
@@ -0,0 +1,99 @@
+from midi_conversion import create_midi
+from staff_detection import \
+    process_image, \
+    detect_staff_lines, \
+    load_features, \
+    find_feature_staffs, \
+    find_pitches, \
+    find_staff_distance, \
+    construct_notes
+from image_operations import \
+    load_image, \
+    save_image, \
+    show_image, \
+    visualize_image, \
+    visualize_staff_lines, \
+    visualize_notes
+from code.note_detection.hough_v2 import note_array
+from staff_removal import staff_removal
+import warnings
+import argparse
+from pathlib import Path
+
+import sys
+
+# sys.path.insert(
+#     1, r"C:\Users\Ivan Zhao\Documents\GitHub\cs1430-final-project\code\note_detection")
+# sys.path.insert(
+#     2, r"C:\Users\Ivan Zhao\Documents\GitHub\cs1430-final-project\code\midi_conversion")
+# sys.path.insert(
+#     3, r"C:\Users\Ivan Zhao\Documents\GitHub\cs1430-final-project\code\preprocessing")
+
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+def command_line_args():
+    parser = argparse.ArgumentParser(
+        description='A program that creates a MIDI file from an image and extracted musical features!')
+
+    parser.add_argument("--image-path",
+                        default='data/fuzzy-wuzzy.png',
+                        type=str,
+                        help="This is the path to your image!")
+    parser.add_argument("--features-path",
+                        default='data/fuzzy_wuzzy_features.csv',
+                        type=str,
+                        help="This is the path to your image's features!")
+    parser.add_argument("--no-vis",
+                        action="store_true",
+                        help="This is a variable representing whether to visualize results or not!")
+
+    return parser.parse_args()
+
+
+def main():
+    args = command_line_args()
+
+    # The current image to process
+    I = process_image(args.image_path)
+
+    staff_lines = detect_staff_lines(I)
+    print("Number of staffs: " + str(staff_lines.shape[0]))
+
+    # A [# of features x 4] array holding all of the features for the image
+    features = load_features(args.features_path)
+
+    # Code Ivan added for feature detection using methods
+    # Function to remove staff
+    removed_staff_img = staff_removal(args.image_path)
+
+    # Function to get Hough
+    detected_circles = note_array(removed_staff_img)
+
+    print(detected_circles)
+
+    matched_staffs = find_feature_staffs(features, staff_lines)
+    print("Matched features to staffs.")
+
+    pitches = find_pitches(features, staff_lines, matched_staffs)
+    print("Matched features to pitches.")
+
+    staff_dist = find_staff_distance(staff_lines)
+
+    if (not(args.no_vis)):
+        visualize_image(I, as_gray=True)
+        visualize_staff_lines(I, staff_lines)
+        visualize_notes(I, features, staff_lines,
+                        matched_staffs, pitches, staff_dist)
+        show_image()
+
+    notes = construct_notes(features, staff_lines, matched_staffs, pitches)
+
+    path = Path(args.image_path).stem
+    create_midi(path, notes)
+
+
+if __name__ == "__main__":
+    main()
--- code/note_detection/hough_v2.py
+++ code/note_detection/hough_v2.py
@@ -6,48 +6,50 @@ from matplotlib import pyplot as plt
 im_size = (850, 1100)
 threshold = im_size[0] * 0.5
 
-#based off Sohum's staff detection
-#needed to find radius parameter for Hough circle
-#returns pixel height between staff lines
+# based off Sohum's staff detection
+# needed to find radius parameter for Hough circle
+# returns pixel height between staff lines
+
+
 def circle_height(image):
-    #modify image
+    # modify image
     inverted_image = util.invert(image)
     horiz_sum = np.sum(inverted_image, axis=1)
     horiz_sum[horiz_sum < 600] = 0
 
-    #maxes for staff lines
+    # maxes for staff lines
     staff_lines = feature.peak_local_max(horiz_sum).flatten()
     staff_lines = np.reshape(staff_lines, (-1, 5))
     staff_lines = np.sort(staff_lines, axis=0)
-    
-    #return just height
-    sorted_indices = np.sort(staff_lines[1,:])
+
+    # return just height
+    sorted_indices = np.sort(staff_lines[1, :])
     height = sorted_indices[1] - sorted_indices[0]
     return height
 
 
-#creates hough circles and returns notehead coordinates
+# creates hough circles and returns notehead coordinates
 def hough_circle(height):
-    #read image
+    # read image
     img = cv2.imread('processed.png', cv2.IMREAD_COLOR)
 
-    #convert to grayscale
+    # convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    #blur using 3 * 3 kernel
+    # blur using 3 * 3 kernel
     gray_blurred = cv2.blur(gray, (3, 3))
 
-    #edges
+    # edges
     edge_detected_image = cv2.Canny(gray_blurred, 75, 200)
 
-    #I HAVE NO TESTED THIS WITH HEIGHT
+    # I HAVE NO TESTED THIS WITH HEIGHT
     detected_circles = cv2.HoughCircles(edge_detected_image,
-                        cv2.HOUGH_GRADIENT, 2, 41, param1 = 100,
-                    param2 = 8, minRadius = 0, maxRadius = height/2)
+                                        cv2.HOUGH_GRADIENT, 2, 41, param1=100,
+                                        param2=8, minRadius=0, maxRadius=height/2)
 
     return detected_circles
 
-    #draw circles that are detected
+    # draw circles that are detected
     # if detected_circles is not None:
 
     #     # Convert the circle parameters a, b and r to integers.
@@ -64,9 +66,11 @@ def hough_circle(height):
     #         cv2.imshow("Detected Circle", img)
     #         cv2.waitKey(0)
 
-#combines both functions
-#THIS IS NOT USING THE GET IMAGE STUFF YET NEEDS TO BE MODIFIED AYEEEEEEEEEEEEEEEEEEEEEEEEEEEE 
-def note_array(path):
-    rawImage = img_as_float32(io.imread('original.png', as_gray=False))
-    height = circle_height(rawImage)
+# combines both functions
+# THIS IS NOT USING THE GET IMAGE STUFF YET NEEDS TO BE MODIFIED AYEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+
+
+def note_array(image):
+    # rawImage = img_as_float32(io.imread('original.png', as_gray=False))
+    height = circle_height(image)
     return hough_circle(height)

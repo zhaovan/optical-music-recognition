f087309b55423cab52cab7e821fe1444a25ab8ae
JusungLee0601 <jusunglee0601@gmail.com>
2020-05-08 17:53:03 -0400

    Hough Comments

--- code/hough_v2.py
+++ code/hough_v2.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 from skimage import color, util, filters, feature, img_as_float32, io
 from matplotlib import pyplot as plt
+from matplotlib import patches
 from staff_detection import \
     process_image, \
     detect_staff_lines, \
@@ -17,8 +18,7 @@ threshold = 500
 # based off Sohum's staff detection
 # needed to find radius parameter for Hough circle
 # returns pixel height between staff lines
-
-
+# exclusively for tests
 def circle_height(image):
     # modify image
     horiz_sum = np.sum(image, axis=1)
@@ -37,7 +37,8 @@ def circle_height(image):
     return height
 
 
-# creates hough circles and returns notehead coordinates
+# creates hough circles and returns notehead coordinates in x, y, r format
+# height: height of each notehead
 def hough_circle(height):
     # read image
     img = cv2.imread('../results/processed.png', cv2.IMREAD_COLOR)
@@ -54,7 +55,7 @@ def hough_circle(height):
     cv2.imshow("Detected Circle", edge_detected_image)
     cv2.waitKey(0)
 
-    # I HAVE NO TESTED THIS WITH HEIGHT
+    # returns all circles
     detected_circles = cv2.HoughCircles(edge_detected_image,
                                         cv2.HOUGH_GRADIENT, 1, 41, param1=100,
                                         param2=9, minRadius=height - 2, maxRadius=height + 4)
@@ -78,12 +79,57 @@ def hough_circle(height):
 
     return detected_circles
 
-# combines both functions
-# THIS IS NOT USING THE GET IMAGE STUFF YET NEEDS TO BE MODIFIED AYEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+# takes bounding boxes, returns list of x, y, r for each notehead for original image
+# img: original image
+# bb: list of bounding boxes
+# height: height of notes
+# simg: staff removed image
+def hough_circle_input(img, bb, height, simg):
+    fig, ax = plt.subplots(1)
+    ax.imshow(simg, cmap='gray_r')
+    for i in range(bb.shape[0]):
+        rect = patches.Rectangle((bb[i, 0], bb[i, 1]), bb[i, 2],
+                                 bb[i, 3], linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+    plt.show()
 
 
+    for i in range(len(bb)):
+        dim = bb[i]
+        cutout = img[dim[1]: (dim[1] + dim[3]), dim[0]: (dim[0] + dim[2])]
+
+        # blur using 3 * 3 kernel
+        gray_blurred = cv2.blur(cutout, (3, 3))
+
+        # edges
+        edge_detected_image = cv2.Canny(cutout, 75, 200)
+
+        # I HAVE NO TESTED THIS WITH HEIGHT
+        detected_circles = cv2.HoughCircles(edge_detected_image,
+                                            cv2.HOUGH_GRADIENT, 1, 41, param1=100,
+                                            param2=9, minRadius=int(height) - 2, maxRadius=int(height) + 4)
+        print(detected_circles)
+
+        # Convert the circle parameters a, b and r to integers.
+        if detected_circles is not None:
+
+            detected_circles = np.uint16(np.around(detected_circles))
+
+            for pt in detected_circles[0, :]:
+                a, b, r = pt[0], pt[1], pt[2]
+
+                # Draw the circumference of the circle.
+                cv2.circle(cutout, (a, b), r, (255, 255, 230), 2)
+
+                # Draw a small circle (of radius 1) to show the center.
+                cv2.circle(cutout, (a, b), 1, (255, 255, 255), 3)
+                cv2.imshow("Detected Circle", cutout)
+                cv2.waitKey(0)
+
+    return detected_circles
+
+
+# originally consolidates all functions, now used to modulate between CNN hough
+# and no CNN hough
 def note_array(height):
-    image = img_as_float32(io.imread('../data/fuzzy-wuzzy.png', as_gray=True))
-    inv_image = util.invert(image)
-    print(height)
     return hough_circle(height)

e4236c0be3b47e2744e381dde60630c849157632
JusungLee0601 <jusunglee0601@gmail.com>
2020-05-08 20:07:42 -0400

    New Images, Hotline

--- code/note_detection/hough_circles.py
+++ code/note_detection/hough_circles.py
@@ -30,22 +30,23 @@ def hough_circle(height):
                                         cv2.HOUGH_GRADIENT, 1, 41, param1=100,
                                         param2=9, minRadius=height - 2, maxRadius=height + 4)
 
-    # draw circles that are detected
-    # if detected_circles is not None:
+    #draw circles that are detected
+    if detected_circles is not None:
 
-    #     # Convert the circle parameters a, b and r to integers.
-    #     detected_circles = np.uint16(np.around(detected_circles))
+        # Convert the circle parameters a, b and r to integers.
+        detected_circles = np.uint16(np.around(detected_circles))
 
-    #     for pt in detected_circles[0, :]:
-    #         a, b, r = pt[0], pt[1], pt[2]
+        # for pt in detected_circles[0, :]:
+        #     a, b, r = pt[0], pt[1], pt[2]
 
-    #         # Draw the circumference of the circle.
-    #         cv2.circle(img, (a, b), r, (0, 255, 0), 2)
+        #     # Draw the circumference of the circle.
+        #     cv2.circle(img, (a, b), r, (0, 255, 0), 2)
 
-    #         # Draw a small circle (of radius 1) to show the center.
-    #         cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
-    #         cv2.imshow("Detected Circle", img)
-    #         cv2.waitKey(0)
+        #     # Draw a small circle (of radius 1) to show the center.
+        #     cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
+        
+        # cv2.imshow("Detected Circle", img)
+        # cv2.waitKey(0)
 
     return detected_circles
 
